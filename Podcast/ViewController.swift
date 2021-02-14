579470fbaa036f626dccb3e817ca7fd3d95653e5
Gin Imor <openheart23@163.com>
2021-02-14 14:27:14 +0800

    Initial Set Up

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Gin Imor on 2/14/21.
+//  Copyright © 2021 Brevity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
